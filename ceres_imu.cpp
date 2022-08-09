6eccf3b6c5aba2d24f2249b67c59bcf733468774
JingJie_Huang <jack810435ge@gmail.com>
2022-08-09 15:59:25 +0800

    add gyro data generation and calibration

--- ceres_imu.cpp
+++ ceres_imu.cpp
@@ -1,84 +1,120 @@
-#include<iostream>
-#include<string>
-#include<fstream>
-#include<sstream>
-#include<ceres/ceres.h>
-#include<chrono>
-#include<vector>
-
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <ceres/ceres.h>
+#include <chrono>
+#include <vector>
 
 using namespace std;
 using namespace ceres;
 
-
-struct IMU_CostFunction{
+struct ACC_CostFunction
+{
 	// constructor
-	IMU_CostFunction(double ax, double ay, double az): ax_(ax), ay_(ay), az_(az) {}
+	ACC_CostFunction(double ax, double ay, double az) : ax_(ax), ay_(ay), az_(az) {}
 	template <typename T>
-	bool operator ()(const T *const para, T *residual) const {
-		//error
-		residual[0] =  9.8*9.8 - (  (para[0]*(ax_ + para[3])) * (para[0]*(ax_ + para[3])) + 
-								    (para[1]*(ay_ + para[4])) * (para[1]*(ay_ + para[4])) + 
-					                (para[2]*(az_ + para[5])) * (para[2]*(az_ + para[5]))  );
+	bool operator()(const T *const para_acc, T *residual) const
+	{
+		T ax_cal = para_acc[0] * ax_ + para_acc[1] * ay_ + para_acc[2] * az_ + para_acc[9];
+		T ay_cal = para_acc[3] * ax_ + para_acc[4] * ay_ + para_acc[5] * az_ + para_acc[10];
+		T az_cal = para_acc[6] * ax_ + para_acc[7] * ay_ + para_acc[8] * az_ + para_acc[11];
+		// define error
+		residual[0] = 9.8 * 9.8 - (ax_cal * ax_cal + ay_cal * ay_cal + az_cal * az_cal);
+
 		return true;
 	}
-	private:
+
+private:
 	const double ax_, ay_, az_;
 };
+struct GYRO_CostFunction
+{
+	// constructor
+	GYRO_CostFunction(double wx, double wy, double wz) : wx_(wx), wy_(wy), wz_(wz) {}
+	template <typename T>
+	bool operator()(const T *const para_gyro, T *residual) const
+	{
+		T wx_cal = wx_ + para_gyro[0];
+		T wy_cal = wy_ + para_gyro[1];
+		T wz_cal = wz_ + para_gyro[2];
+		// define error
+		residual[0] = wx_cal * wx_cal + wy_cal * wy_cal + wz_cal * wz_cal;
 
+		return true;
+	}
+
+private:
+	const double wx_, wy_, wz_;
+};
 
 int main(int argc, char *argv[])
 {
 	vector<vector<double>> matrix;
-	//readfile
+	// readfile
 	ifstream file;
+	if (argc != 2)
+	{
+		cout << "Using example: " << endl;
+		cout << "./Ceres_IMU_Calibration ../imu_data.csv" << endl;
+		return 0;
+	}
 	cout << "----- Fetching .csv file ..." << endl;
-	file.open("../imu_data.csv");
-	if(!file.is_open()){
+	file.open(argv[1]);
+	if (!file.is_open())
+	{
 		cerr << "ERROR: failed to open the file!" << endl;
 		return 1;
 	}
 	string line;
-	while (getline( file, line,'\n'))  //讀檔讀到跳行字元
+	while (getline(file, line, '\n')) //讀檔讀到跳行字元
 	{
 		vector<double> row_data;
 		stringstream templine(line); // string 轉換成 stream
 		string data;
-		 while (getline( templine, data,',')) //讀檔讀到逗號
-		 {
-			 row_data.push_back(stof(data));  //string 轉換成數字
-		 }
+		while (getline(templine, data, ',')) //讀檔讀到逗號
+		{
+			row_data.push_back(stof(data)); // string 轉換成數字
+		}
 		matrix.push_back(row_data);
 	}
 	file.close();
-	cout << "----- Finish!" << endl << endl;
+	cout << "----- Finish!" << endl
+		 << endl;
 	cout << "Whether to show the raw data on the SCREEN? 'y' or 'n': ";
 	char resp;
 	cin >> resp;
-	if(resp == 'y'){
+	if (resp == 'y')
+	{
 		cout << "row_data: " << endl;
-		for(int i=0; i<matrix.size(); i++){
-			for(int j=0; j<matrix[i].size(); j++){
+		for (int i = 0; i < matrix.size(); i++)
+		{
+			for (int j = 0; j < matrix[i].size(); j++)
+			{
 				cout << matrix[i][j] << "  ";
 			}
 			cout << endl;
 		}
 	}
 
-	cout << "----- Calling Ceres solver to calibrate the imu data" << endl << endl;
-	// ceres solver to calcualte imu calibration parameters [sx, sy, sz, bx, by, bz]
+	cout << "----- Calling Ceres solver to calibrate the accelerometer" << endl
+		 << endl;
+	// ceres solver to calcualte acc calibration parameters [s11, s12, s13, s21, s22, s23, s31, s32, s33, bx, by, bz]
 
-	double para[6] = {1.0, 1.0, 1.0, 0.0, 0.0, 0.0};
-	
+	double para_acc[12] = {1.0, 0.0, 0.0,
+						   0.0, 1.0, 0.0,
+						   0.0, 0.0, 1.0,
+						   0.0, 0.0, 0.0};
+
 	Problem problem;
 
-	for(int k=0; k<matrix.size(); k++){
-		CostFunction *cost_function = new AutoDiffCostFunction<IMU_CostFunction,1,6>(
-		new IMU_CostFunction(matrix[k][0], matrix[k][1], matrix[k][2])  );
-		problem.AddResidualBlock(cost_function, new CauchyLoss(0.5), para);
+	for (int k = 0; k < matrix.size(); k++)
+	{
+		CostFunction *cost_function = new AutoDiffCostFunction<ACC_CostFunction, 1, 12>(
+			new ACC_CostFunction(matrix[k][0], matrix[k][1], matrix[k][2]));
+		problem.AddResidualBlock(cost_function, new CauchyLoss(0.5), para_acc);
 	}
 	// using Cauchy function to reduce the influence of the outlier inside the data
-
 	Solver::Options options;
 	options.linear_solver_type = ceres::DENSE_QR;
 	options.max_num_iterations = 50;
@@ -88,23 +124,61 @@ int main(int argc, char *argv[])
 
 	cout << summary.FullReport() << endl;
 	cout << "----- Output the result:" << endl;
-	cout << "	The mathematical model: " << endl;
-	cout << "	ax_cal = sx * (ax_raw + ba)" << endl
-		 << "	ay_cal = sy * (ay_raw + by)" << endl
-		 << "	az_cal = sz * (az_raw + bz)" << endl << endl;
-	cout << "	sx = " << para[0] << endl;
-	cout << "	sy = " << para[1] << endl;
-	cout << "	sz = " << para[2] << endl;
-	cout << "	bx = " << para[3] << endl;
-	cout << "	by = " << para[4] << endl;
-	cout << "	bz = " << para[5] << endl;
-	
+	cout << "The mathematical model: " << endl;
+	cout << "ax_cal = s11 * ax_raw + s12 * ay_raw + s13 * az_raw + bx" << endl
+		 << "ay_cal = s21 * ax_raw + s22 * ay_raw + s23 * az_raw + by" << endl
+		 << "az_cal = s31 * ax_raw + s32 * ay_raw + s33 * az_raw + bz" << endl
+		 << endl;
+	cout << "========== Accelerometer calibration parameters output ==========" << endl;
+	cout << "=> Scaling matrix: " << endl;
+	printf("%10.4f", para_acc[0]);
+	printf("%10.4f", para_acc[1]);
+	printf("%10.4f\n", para_acc[2]);
+	printf("%10.4f", para_acc[3]);
+	printf("%10.4f", para_acc[4]);
+	printf("%10.4f\n", para_acc[5]);
+	printf("%10.4f", para_acc[6]);
+	printf("%10.4f", para_acc[7]);
+	printf("%10.4f\n", para_acc[8]);
+	cout << endl
+		 << "=> Bias vector: " << endl;
+	printf("%10.4f\n", para_acc[9]);
+	printf("%10.4f\n", para_acc[10]);
+	printf("%10.4f\n", para_acc[11]);
+
+	cout << endl
+		 << endl;
+	cout << "----- Calling Ceres solver to calibrate the gyroscope" << endl
+		 << endl;
+	// ceres solver to calcualte gyro calibration parameters [bx, by, bz]
+
+	double para_gyro[3] = {0.0, 0.0, 0.0};
+
+	for (int k = 0; k < matrix.size(); k++)
+	{
+		CostFunction *cost_function = new AutoDiffCostFunction<GYRO_CostFunction, 1, 3>(
+			new GYRO_CostFunction(matrix[k][3], matrix[k][4], matrix[k][5]));
+		problem.AddResidualBlock(cost_function, new CauchyLoss(0.5), para_gyro);
+	}
+	// using Cauchy function to reduce the influence of the outlier inside the data
+	options.linear_solver_type = ceres::DENSE_QR;
+	options.max_num_iterations = 50;
+	options.minimizer_progress_to_stdout = true;
+	Solve(options, &problem, &summary);
+
+	cout << summary.FullReport() << endl;
+	cout << "----- Output the result:" << endl;
+	cout << "The mathematical model: " << endl;
+	cout << "wx_raw + bx" << endl
+		 << "wy_raw + by" << endl
+		 << "wz_raw + bz" << endl
+		 << endl;
+	cout << "========== Gyroscope calibration parameters output ==========" << endl;
+
+	cout << "=> Bias vector: " << endl;
+	printf("%10.4f\n", para_gyro[0]);
+	printf("%10.4f\n", para_gyro[1]);
+	printf("%10.4f\n", para_gyro[2]);
 
 	return 0;
 }
-
-
-
-
-
-
--- imu_data_gen.py
+++ imu_data_gen.py
@@ -11,13 +11,13 @@ x = []
 y = []
 z = []
 # scale factor to calibrate
-sx = 1.05
-sy = 0.94
-sz = 0.96
+scale = np.array([[0.98, -0.02, 0.05],
+                  [-0.07, 0.97, 0.04],
+                  [-0.02, 0.08, 1.02]])
 # bias factor to calibrate
-bx = 3.5
-by = -2.2
-bz = 4.7
+bias =np.array([[3.5],
+                [-2.2],
+                [4.7]])
 
 def set_axes_equal(ax):
     '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
@@ -27,7 +27,6 @@ def set_axes_equal(ax):
     Input
       ax: a matplotlib axis, e.g., as output from plt.gca().
     '''
-
     x_limits = ax.get_xlim3d()
     y_limits = ax.get_ylim3d()
     z_limits = ax.get_zlim3d()
@@ -47,34 +46,44 @@ def set_axes_equal(ax):
     ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
     ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
 
-for phi in np.linspace(0,2*math.pi,25):
-	for theta in np.linspace(-0.5*math.pi,0.5*math.pi,25):
+# generation of accelerometer raw data (which contain scaling, bias and noise)
+for phi in np.linspace(0,2*math.pi,10):
+	for theta in np.linspace(-0.5*math.pi,0.5*math.pi,10):
 		x.append(g*math.cos(theta)*math.cos(phi))
 		y.append(g*math.cos(theta)*math.sin(phi))
 		z.append(g*math.sin(theta))
 
-# add some outlier
-
+# add some outlier in accelerometer raw data
 for phi in np.linspace(0,0.4*math.pi,3):
 	for theta in np.linspace(-0.3*math.pi,0.2*math.pi,3):
 		x.append(2.5*g*math.cos(theta)*math.cos(phi))
 		y.append(3.4*g*math.cos(theta)*math.sin(phi))
-		z.append(2.7*g*math.sin(theta))
-
+		z.append(4.7*g*math.sin(theta))
 
 
 N = len(x)
-# artificial IMU (with noise) data generation
-x = np.array(x)
-y = np.array(y)
-z = np.array(z)
+x = np.array(x).reshape(1, N)
+y = np.array(y).reshape(1, N)
+z = np.array(z).reshape(1, N)
 print(type(x))
-noise = 0.1*np.random.randn(N)
-print(type(noise))
-Ax = (1.0/sx)*x - bx + noise
-Ay = (1.0/sy)*y - by + noise
-Az = (1.0/sz)*z - bz + noise
+Acc_cal = np.concatenate((x, y, z), axis = 0)
+print(Acc_cal.shape)
 
+noise_x = 0.2*np.random.randn(N).reshape(1,N)
+noise_y = 0.2*np.random.randn(N).reshape(1,N)
+noise_z = 0.2*np.random.randn(N).reshape(1,N)
+noise = np.concatenate((noise_x, noise_y, noise_z), axis = 0)
+# add bias to acc
+Acc_cal[0] = Acc_cal[0]-bias[0][0]
+Acc_cal[1] = Acc_cal[1]-bias[1][0]
+Acc_cal[2] = Acc_cal[2]-bias[2][0]
+# add scale to acc
+Acc_raw = np.matmul(np.linalg.inv(scale), Acc_cal)
+# add noise to accelerometer raw data
+Acc_raw = np.add(Acc_raw, noise)
+Ax = Acc_raw[0]
+Ay = Acc_raw[1]
+Az = Acc_raw[2]
 
 ax.scatter(x,y,z, s=1, marker=',', color='b', label='Ideal data')
 ax.scatter(Ax,Ay,Az, s=1, marker=',', color='r', label='Artificial data')
@@ -85,21 +94,46 @@ ax.set_xlim(-25,25)
 ax.set_ylim(-25,25)
 ax.set_zlim(-25,25)
 set_axes_equal(ax)
+
+
+# generation of gyroscope raw data (which contain bias and noise)
+bais_gyro = np.array([[0.1],
+                      [-0.12],
+                      [0.15]])
+# add bias to gyro
+Wx = np.zeros((N,), dtype=float) - bais_gyro[0][0]
+Wy = np.zeros((N,), dtype=float) - bais_gyro[1][0]
+Wz = np.zeros((N,), dtype=float) - bais_gyro[2][0]
+# add noise to gyroscope raw data
+noise_x = 0.02*np.random.randn(N)
+noise_y = 0.02*np.random.randn(N)
+noise_z = 0.02*np.random.randn(N)
+Wx = np.add(Wx, noise_x)
+Wy = np.add(Wy, noise_y)
+Wz = np.add(Wz, noise_z)
+# add some outliers in gyro raw data 
+Wx[10] = 3.1
+Wy[10] = -4.5
+Wz[10] = -3.6
+Wx[20] = 5.6
+Wy[20] = -2.1
+Wz[20] = -1.6
+
+# display gyroscope raw data
+plt.figure()
+plt.plot(Wx,'r', Wy, 'g', Wz, 'b')
+plt.xlabel('Number of data')
+plt.ylabel('rad/sec')
+plt.legend(["Wx", "Wy", "Wz"],loc = "lower right")
 plt.show()
 
-# save generated data to .csv file
-Data = np.transpose(np.asarray([Ax,Ay,Az]))
-#Data = np.asarray([[1,2,3], [4,5,6], [7,8,9]])
 print(np.shape(Ax))
+print(np.shape(Wx))
+# Save generated data to .csv file
+Data = np.transpose(np.asarray([Ax,Ay,Az,Wx,Wy,Wz]))
 print(np.shape(Data))
-#Data.tofile('./imu_data.csv', sep=',', format='%6.3f')
-np.savetxt('./imu_data.csv', Data, delimiter=',', fmt='%6.4f')
-
-
-
-
-
-
+# Data.tofile('./imu_data.csv', sep=',', format='%6.3f')
+np.savetxt('./imu_data.csv', Data, delimiter=',', fmt='%8.5f')
 
 
 
